d740cf9789eb7ca9d519b1c5509282769694c1ef
M. Adil Fayyaz <adilfayyaz6@gmail.com>
2022-03-28 19:49:30 +0500

    Location adjectives change to proper nouns

--- Inferencers/spacy_pipeline/relation_inferencer.py
+++ Inferencers/spacy_pipeline/relation_inferencer.py
@@ -10,6 +10,17 @@ from stix2 import *
 import requests, json, os
 from elasticsearch import Elasticsearch
 # pip install elasticsearch==5.5.3
+# pip install nltk
+# pip install pycountry
+
+import re
+import nltk
+import pycountry
+
+countries = [country.name for country in pycountry.countries]
+countries = [x.lower() for x in countries]
+countries.append('russia') # Russia is properly known as russian frederation.
+
 
 desired_width = 320
 
@@ -92,6 +103,63 @@ def makeStixBundle(prediction):
   return bundle.serialize(pretty=True)
   
 
+def convertLocation(word):
+  # list of regex things to check
+  patterns = ['ese', 'ian', 'an', 'ean', 'n', 'ic', 'ern', 'i']
+	#list of suffixes for appending to country names that get damaged when they are split.
+  suffixes = ['a', 'o']
+
+	# for every potential way of forming a nationality adjective, test them.
+  for pattern in patterns:
+    tup = re.findall(r'^(.*)(' + pattern + ')', word)
+    if tup:
+      country = tup[0][0]
+
+      # check to see if the country is in the list of countries returned by pycountry. If it is, return it.
+      
+      if country in countries:
+        return country
+
+      # if the stem is not a country, try adding suffixes to it to see if you can pull out a real country.
+      else:
+        for suffix in suffixes:
+          new_country = country + suffix
+          if new_country in countries:
+            return new_country
+
+
+def fixLocationAdjectives(prediction):
+  entities = prediction["entity tags"]
+  relations = prediction["relationships"]
+  locs = []
+  loc_found = 0
+  for i,e in enumerate(entities[0]):
+    if e[2]=="L":
+      loc_found = 1
+      locs.append(e[1])
+      new_country = convertLocation(e[1])
+      if new_country!=e[1]:
+        add_list = [e[0],new_country,e[2]]
+        prediction["entity tags"][0][i] = add_list
+  
+  # Fix relationships
+  if loc_found:
+    for i, val in enumerate(relations):
+      if val["entities"][0] in locs:
+        new_country = convertLocation(val["entities"][0])
+        if new_country!=val["entities"][0]:     
+          add_list = [new_country,val["entities"][1]]
+          prediction["relationships"][i]["entities"] = add_list
+      
+      if val["entities"][1] in locs:
+        new_country = convertLocation(val["entities"][1])
+        if new_country!=val["entities"][1]:     
+          add_list = [val["entities"][0], new_country]
+          prediction["relationships"][i]["entities"] = add_list
+
+  return prediction
+
+	
 # bertModel = BertTokenClassification()
 relationExtracter=RelationExtracter()
 @app.post("/getInference")
@@ -110,11 +178,78 @@ async def get_model_inference(req: Request):
     else:
       x= sentence.lower()
       text.append(x)
-    
+
     prediction = relationExtracter.getInference(text)
+    # Fix location adjectives to nouns
+    prediction = fixLocationAdjectives(prediction)
+
     return prediction
     
 @app.post("/getBundle")
 async def get_stix_bundle(req: Request):
   prediction = await req.json()
   return makeStixBundle(prediction)
+
+
+def makeStixBundle(finalBundle):
+  entities = finalBundle["entities"]
+  relations = finalBundle["relationships"]
+  
+  entities_list = []
+
+
+  for e in entities:
+    if(e["type"] == "threat-actor"):
+      entities_list.append(ThreatActor(name=e["name"]))
+    elif(e["type"] == "malware"):
+      entities_list.append(Malware(name=e["name"], is_family = False))
+    elif(e["type"] == "indicator"):
+      entities_list.append(Indicator(name=e["name"],pattern="[user-account:value = 'none']", pattern_type="stix"))
+    elif(e["type"] == "identity"):
+      entities_list.append(Identity(name=e["name"]))
+    elif(e["type"] == "tool"):
+      entities_list.append(Tool(name=e["name"]))
+    elif(e["type"] == "vulnerability"):
+      entities_list.append(Vulnerability(name=e["name"]))
+    elif(e["type"] == "location"):
+      entities_list.append(Location(name=e["name"], longitude=0.0, latitude=0.0,region="", country=""))
+    elif(e["type"]=="infrastructure"):
+      entities_list.append(Infrastructure(name=e["name"]))
+    elif(e["type"]=="campaign"):
+      entities_list.append(Campaign(name=e["name"]))
+
+  a_list = []
+  global a
+  a= None 
+  global b
+  b= None 
+  for val in relations:
+    for ent in entities_list:
+        # Find the entity match
+        if (val["source"] == ent["name"]):
+          a = ent
+        if (val["target"] == ent["name"]):
+          b = ent
+          
+        # Found both entities
+        if a and b:
+          relationship = Relationship(relationship_type=val["name"],
+          source_ref=a,
+          target_ref=b)
+          if a not in a_list:
+            a_list.append(a)
+          if b not in a_list:
+            a_list.append(b)
+          a_list.append(relationship)
+          a= None
+          b=None
+          
+  bundle = Bundle(a_list)
+ 
+
+  return bundle.serialize(pretty=True)
+  
+@app.post("/makeStix")
+async def make_stix_bundle(req: Request):
+	finalBundle = await req.json()
+	return makeStixBundle(finalBundle)
