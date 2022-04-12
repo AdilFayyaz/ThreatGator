0054dbf8f3c673ce47b1070a91fb0a197e42d8aa
hurriya nasir <hurriyanasir@gmail.com>
2022-04-12 11:08:31 +0500

    merged reports frontend

--- Front End/src/views/sidebar/latest/Report.js
+++ Front End/src/views/sidebar/latest/Report.js
@@ -23,16 +23,18 @@ const Reports = (props) => {
   const [reportsData, SetReportsData] = useState({})
   const [hash1, SetHash] = useState(0)
   const [threatScore, SetThreatScore] = useState('')
+  const [mergedReports, setMergedReports] = useState([])
   // fetching data from data analysis service for reports
   const location = useLocation()
   const hash = location.state.hash
   // const org_id=location.state.org_id
-
+  const mergedReportsBundles = []
   console.log('hash' + hash)
+  const data1 = { org_id: location.state.org_id, reportId: hash, index: 'tagged_bundle_data' }
 
   function getScore() {
     // return null
-    const data1 = { org_id: location.state.org_id, reportId: hash, index: 'tagged_bundle_data' }
+    // const data1 = { org_id: location.state.org_id, reportId: hash, index: 'tagged_bundle_data' }
 
     var myHeaders = new Headers()
     myHeaders.append('Content-Type', 'text/plain')
@@ -62,15 +64,67 @@ const Reports = (props) => {
 
   function getStix() {
     console.log('getting stix')
-    fetch('http://127.0.0.1:8082/dataAnalysis/getStixBundle/' + hash + '/tagged_bundle_data')
-      .then((res) => res.json())
-      .then((data) => {
-        SetHash(data)
-      })
-    console.log('abc' + hash1.id)
+    // fetch('http://127.0.0.1:8082/threatScore/filterStixBundle/' + hash + '/tagged_bundle_data')
+    //   .then((res) => res.json())
+    //   .then((data) => {
+    //     SetHash(data)
+    //   })
+    // console.log('abc' + hash1.id)
+    var requestOptions = {
+      method: 'GET',
+      redirect: 'follow',
+    }
+
+    fetch(
+      'http://127.0.0.1:8086/threatScore/filterStixBundle/?org_id=' +
+        encodeURIComponent(data1.org_id) +
+        '&report_id=' +
+        encodeURIComponent(data1.reportId) +
+        '&index=' +
+        encodeURIComponent(data1.index),
+      requestOptions,
+    )
+      .then((response) => response.text())
+      .then((result) => SetHash(result))
+      .catch((error) => console.log('error', error))
+
+    //  get merged reports
+    var myHeaders = new Headers()
+    myHeaders.append('Content-Type', 'application/json')
+
+    var raw = JSON.stringify({
+      hash: hash,
+    })
+
+    var requestOptions = {
+      method: 'POST',
+      headers: myHeaders,
+      body: raw,
+      redirect: 'follow',
+    }
+
+    fetch('http://127.0.0.1:8082/dataAnalysis/getRelatedReports', requestOptions)
+      .then((response) => response.text())
+      .then((result) => setMergedReports(result))
+      .catch((error) => console.log('error', error))
+
+    if (mergedReports != []) {
+      for (var i = 0; i < mergedReports.length; i++) {
+        fetch(
+          'http://127.0.0.1:8082/threatScore/filterStixBundle/' +
+            mergedReports[i].id +
+            mergedReports[i].index,
+        )
+          .then((res) => res.json())
+          .then((data) => {
+            mergedReportsBundles.push(data)
+          })
+      }
+      console.log('merged reports!!!! ' + mergedReportsBundles)
+    }
   }
   useEffect(() => {
-    // getStix()
+    getStix()
     getScore()
     return () => {
       console.log('returning ')
